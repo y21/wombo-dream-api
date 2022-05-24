c04803f6415e4d4f251e1a22dbfaeeb2835dc97f
y21 <btimo8822@gmail.com>
2022-05-24 22:08:13 +0200

    impl server

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import polka from 'polka';
+import http from 'http';
+import * as WomboDream from './index';
+
+const port = process.env.PORT || 3030;
+
+console.log(`Listening on port ${port}`);
+
+function reject(res: http.ServerResponse, status: number, message: string) {
+    res.writeHead(status);
+    res.end(message);
+}
+
+polka()
+    .get('/generate', async (req, res) => {
+        const { style, message } = req.query;
+
+        if (Number.isNaN(style) || typeof message !== 'string') {
+            return reject(res, 400, 'Malformed query parameters');
+        }
+
+        const wombo = WomboDream.buildDefaultInstance();
+        try {
+            const task = await wombo.generatePicture(message, Number(style));
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(task));
+        } catch (e) {
+            console.log(e);
+            return reject(res, 500, 'Failed to process wombo request');
+        }
+    })
+    .listen(port);
